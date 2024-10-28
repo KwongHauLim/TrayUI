923b3dc29cb501c7724f20488fe67d8a02d93f57
Kwong Hau Lim <haulim.kwong@gmail.com>
2024-10-28 12:25:58 +0800

    Tray window

---
+++ TrayUI/NoWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrayUI
+{
+    /// <summary>
+    /// Interaction logic for NoWindow.xaml
+    /// </summary>
+    public partial class NoWindow : Window
+    {
+        public NoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+            Visibility = Visibility.Hidden;
+        }
+
+        public static NoWindow Create(Icon tray)
+        {
+            var window = new NoWindow();
+            window.Content = tray;
+            window.Show();
+            return window;
+        }
+    }
+}
